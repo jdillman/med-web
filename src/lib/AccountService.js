eacf343e759ec686aacfaed2150903ef592449b9
jdillman <jdd619@gmail.com>
2019-05-21 21:50:05 -0700

    dot files

--- src/lib/AccountService.js
+++ src/lib/AccountService.js
@@ -1,69 +1,38 @@
-import { wrapService, withPostSuccess } from 'redux-recompose';
+import { createTypes, createReducer, completeState } from 'redux-recompose';
 
-const initialState = {
-  targetLoading: true
-};
+const initialState = completeState({
+  accounts: [],
+});
 
-const reducerDescription = {
-  [actions.LOADED]: onLoaded()
-};
+const actions = createTypes([
+  'CREATE',
+  'READ',
+  'UPDATE',
+  'DELETE',
+], '@@ACCOUNT');
 
-export default createReducer(initialState, reducerDescription);
-
-
-
-
-
-
-class AccountService {
-  constructor() {
-    this.items = [
-      {
-        account_id: 1,
-        name: "test1",
-        active: false,
-        active_at: null,
-      },
-      {
-        account_id: 1,
-        name: "test1",
-        active: false,
-        active_at: null,
-      },
-    ];
-  }
-
-  async getAll() {
-    return Promise.resolve(this.items);
-  }
-
-  async get(id) {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].id === id) {
-        return Promise.resolve(this.items[i]);
-      }
-    }
-
-    return null;
-  }
-
-  async create(item) {
-    console.log("ItemService.createItem():");
-    console.log(item);
-    return Promise.resolve(item);
-  }
-
-  async deleteItem(itemId) {
-    console.log("ItemService.deleteItem():");
-    console.log("item ID:" + itemId);
-  }
-
-  async updateItem(item) {
-    console.log("ItemService.updateItem():");
-    console.log(item);
-  }
+const reducerDescription: {
+  primaryActions: [actions.CREATE, actions.READ, actions.UPDATE, actions.DELETE],
+  override: {
+    // [actions.custom]: onAdd()
+  },
 }
 
-// Export your service by also specifying the reducer name and the target for each action.
-export default wrapService(service, 'account', { getCards: 'cards' });
+// 
+// function transformArr(arr, key) {
+//   const ordered = [];
+//   const byId = arr.reduce((acc, obj) => {
+//     ordered.push(obj[key]);
+//     return {
+//       ...acc,
+//       [obj[key]]: obj,
+//     };
+//   }, {});
+// 
+//   return {
+//     ordered,
+//     byId,
+//   };
+// }
 
+export default createReducer(initialState, completeReducer(reducerDescription))
